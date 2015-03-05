324d0b44c5701198dfe112b17487d89ea2dfad07
mmikel <mmikel@Lubuntu>
2015-03-05 02:00:25 +0100

    OSOA

--- src/com/zubiri/multiteca/Main.java
+++ src/com/zubiri/multiteca/Main.java
@@ -8,53 +8,60 @@ public final class Main {
 
 		public static void main(String[] args) throws IOException  {
 			
-			int seleccion = -1;
+			int opcion = -1;
 			Scanner sc = new Scanner(System.in);
 			Multiteca multiteca = null;
 			
 			if (args.length == 0) {
+				
 				System.out.println("El numero de argumentos no es valido:");
 				System.out.println("java main.java <fichero_artistas>");
 				System.exit(-1);
 			} 
-	    	//Leemos la lista de artistas del fichero.
+			
+	    	//Leer  fichero artistas
 			Artistas.leerArtistas(args[0]);
 
 			do {
 				try {
-				//Visualiza por terminal (stdout) las opciones del men
-				System.out.println("Mostrar Artistas-----------------------------1");
-				System.out.println("Solicitar obras------------------------------2");
-				System.out.println("Mostrar obras--------------------------------3");
-				System.out.println("SALIR DEL PROGRAMA---------------------------4");
-				   
-				seleccion = sc.nextInt();
-				switch (seleccion) {
-					
-					case 1: //Mostrar artistas
-						Artistas.mostrarArtistas();
-						break;
+					//Visualizar menu principal
+					System.out.println("Mostrar artistas-----------------------------1");
+					System.out.println("Solicitar obras------------------------------2");
+					System.out.println("Mostrar obras--------------------------------3");
+					System.out.println("Salir del programa---------------------------4");
+					   
+					opcion = sc.nextInt();
+					switch (opcion) {
 						
-					case 2: //Solicitar obras
-						multiteca = new Multiteca(sc);
-						break;
-					case 3: //Mostrar  obras
-						if (multiteca == null) {
-							System.out.println("No es posible mostrar las obras sin haber sido solicitados");
-						}
-						multiteca.mostrarMultiteca();
-						break;
-					case 4: //Salimos
-						break;
-					default:
-						System.out.println("No ha insertado la opcion correcta");
-				}
-				}catch(InputMismatchException exception){
-					System.out.println("Eso no es numero");
+						case 1: //Mostrar artistas
+							Artistas.mostrarArtistas();
+							break;
+							
+						case 2: //Solicitar obras
+							multiteca = new Multiteca(sc);
+							break;
+						case 3: //Mostrar  obras
+							if (multiteca == null) {
+								System.out.println("No es posible mostrar las obras sin haber sido solicitados");
+							}
+							multiteca.mostrarMultiteca();
+							break;
+						case 4: 
+							System.out.println("Saliendo del programa Multiteca");
+							System.exit(-1);
+							break;
+						default:
+							System.out.println("No ha insertado la opcion correcta");
+					}
+					
+				}catch(InputMismatchException ime){
+					
+					System.out.println("Error, No has introducido un numero, mete un numero del menu");
 					sc.nextLine();
 				}
-			} while (seleccion != 4);
-			System.out.println("Gracias por usar nuestro programa. Que tenga un buen dia!");
+				
+			} while (opcion != 4);
+			
 			sc.close();
 		}
 		
